dbd2a5a1d9fef4d5ec70dc3f085ff133327da9f1
Alex Kaplan <alex@nextthing.co>
2016-12-09 19:10:45 -0800

    first commit

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt-get update && \
+
+sudo apt-get install \
+build-essentials \
+libsdl1.2-dev \
+git
+
+git clone https://github.com/xobs/mcrpi-wrapper
+mkdir mcrpi-wrapper
+pushd mcrpi-wrapper
+git apply ../0001-set-PocketCHIP-screen-resolution.patch
+make
+popd
+
+git clone https://github.com/thp/eglo
+mkdir eglo
+pushd eglo
+make
+popd
+
+wget https://s3.amazonaws.com/assets.minecraft.net/pi/minecraft-pi-0.1.1.tar.gz
+tar xf minecraft-pi-0.1.1.tar.gz
+pushd mcpi
+cp ../mcrpi-wrapper/libbcm_host.so.1.0 libbcm_host.so
+cp ../eglo/libeglo.so libGLESv2.so
+ln -s libGLESv2.so libGLESv2.so
+cp ../start.sh .
+
---
+++ start.sh
@@ -0,0 +1,3 @@
+export LD_PRELOAD=./libbcm_host.so
+export LD_LIBRARY_PATH=$PWD
+./minecraft-pi
